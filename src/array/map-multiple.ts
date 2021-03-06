a084b63f58406483383037c55959363d4cdc5ed2
Pete Smith <pete@petesmith.io>
2021-03-06 18:54:43 +0000

    Implements mapMultiple

--- src/array/map-multiple.ts
+++ src/array/map-multiple.ts
@@ -2,8 +2,8 @@
  *
  */
 
-type T = () => string;
+type T = <A extends unknown, B extends unknown, C extends unknown>(cb: (a: A, b: B) => C, first: A[], second: B[]) => C[];
 
-const f: T = () => 'map-multiple';
+const f: T = (callback, first, second) => first.map((_, index) => callback(first[index], second[index]));
 
 export default f;
