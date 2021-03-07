af8a944afa5f1b231f00b572035b624808c51d06
Pete Smith <pete@petesmith.io>
2021-03-07 21:29:32 +0000

    Implements array/join

--- src/array/join.spec.ts
+++ src/array/join.spec.ts
@@ -1,7 +1,11 @@
 import join from './join';
 
 describe('array/join', () => {
-    it('does something', () => {
-        expect(join()).toEqual('join');
+    it('joins a string array with the given separator', () => {
+        expect(join('&', ['one', 'two', 'three'])).toEqual('one&two&three');
+    });
+
+    it('is curried', () => {
+        expect(join('-')(['a', 'b', 'c'])).toEqual('a-b-c');
     });
 });
--- src/array/join.ts
+++ src/array/join.ts
@@ -2,8 +2,11 @@
  *
  */
 
-type T = () => string;
+import Curried from '../core/type/curried';
+import curry from '../core/curry';
 
-const f: T = () => 'join';
+type T = Curried<(separator: string, array: string[]) => string>;
+
+const f: T = curry((separator, array) => array.join(separator));
 
 export default f;
