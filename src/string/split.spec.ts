99364bfbe205d917fecc28544b12d7181919ac8b
Pete Smith <pete@petesmith.io>
2021-03-11 22:03:06 +0000

    Adds string functions
    string/split
    string/splitBefore
    string/concat

--- src/string/split.spec.ts
+++ src/string/split.spec.ts
@@ -1,7 +1,21 @@
 import split from './split';
 
 describe('string/split', () => {
-    it('splits string by any occurence of the provided delimiter', () => {
-        expect(split('abc', 'oneabctwoabcthree')).toEqual(['one', 'two', 'three']);
+    it('splits string by any occurrence of the provided string delimiter', () => {
+        const value = 'oneabctwoabcthree';
+        const delimiter = 'abc';
+
+        expect(split(delimiter, value)).toEqual(['one', 'two', 'three']);
+    });
+
+    it('splits string by any occurence of the provided regex delimiter', () => {
+        const value = 'oneAtwoABCthree';
+        const delimiter = /[A-Z]+/;
+
+        expect(split(delimiter, value)).toEqual(['one', 'two', 'three']);
+    });
+
+    it('is curried', () => {
+        expect(split('-')('one-two')).toEqual(['one', 'two']);
     });
 });
