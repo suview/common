b6e1506c2486f89783fd68430fc74c6d1ea11dd4
Pete Smith <pete@petesmith.io>
2021-03-18 10:55:18 +0000

    Implements array/map

--- src/array/map.ts
+++ src/array/map.ts
@@ -2,8 +2,8 @@
  *
  */
 
-type T = () => string;
+type T = <A extends unknown, B extends unknown>(callback: (a: A) => B) => (array: A[]) => B[];
 
-const f: T = () => 'map';
+const f: T = callback => array => array.map(callback);
 
 export default f;
