d4716f67de08fd65d30d2a96e52d9ac5019ae2dc
Pete Smith <pete@petesmith.io>
2021-03-18 10:55:18 +0000

    Implements array/map

--- src/array/map.spec.ts
+++ src/array/map.spec.ts
@@ -1,7 +1,13 @@
 import map from './map';
 
 describe('array/map', () => {
-    it('does something', () => {
-        expect(map()).toEqual('map');
+    it('uses native array map', () => {
+        const array = [1, 2, 3];
+        const callback = (num: number) => num * num;
+
+        expect(map(callback)(array)).toEqual([1, 4, 9]);
     });
+
+    // TODO Currently manually curried due to
+    // https://github.com/millsp/ts-toolbelt/issues/207
 });
