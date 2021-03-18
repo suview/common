327b7f0a89cc903d438d231be107e707727f5685
Pete Smith <pete@petesmith.io>
2021-03-18 10:55:44 +0000

    Implements array/reduce

--- src/array/reduce.spec.ts
+++ src/array/reduce.spec.ts
@@ -1,7 +1,13 @@
 import reduce from './reduce';
 
 describe('array/reduce', () => {
-    it('does something', () => {
-        expect(reduce()).toEqual('reduce');
+    it('uses native array reduce', () => {
+        const array = [1, 2, 3];
+        const callback = (a: number, b: number) => a + b;
+
+        expect(reduce(callback, 0)(array)).toEqual(6);
     });
+
+    // TODO Currently manually curried due to
+    // https://github.com/millsp/ts-toolbelt/issues/207
 });
--- src/array/reduce.ts
+++ src/array/reduce.ts
@@ -2,8 +2,8 @@
  *
  */
 
-type T = () => string;
+type T = <A, B>(callback: (acc: B, next: A) => B, initialValue: B) => (array: A[]) => B;
 
-const f: T = () => 'reduce';
+const f: T = (callback, initialValue) => array => array.reduce(callback, initialValue);
 
 export default f;
