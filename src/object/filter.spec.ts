90403b2999dcd7c1e466db9bdfbf44f81101b07d
Pete Smith <pete@petesmith.io>
2021-04-08 13:31:20 +0100

    Implements object/filter

--- src/object/filter.spec.ts
+++ src/object/filter.spec.ts
@@ -1,7 +1,21 @@
 import filter from './filter';
 
 describe('object/filter', () => {
-    it('does something', () => {
-        expect(filter()).toEqual('filter');
+    it('returns subset of object values which match the provided predicate', () => {
+        const isEven = (x: number) => x % 2 === 0;
+        const obj = { one: 1, two: 2, three: 3, four: 4 };
+        const expectedObj = { two: 2, four: 4 };
+
+        expect(filter(isEven)(obj)).toEqual(expectedObj);
     });
+
+    it('returns empty object if no matches are found', () => {
+        const isGreateThanTen = (x: number) => x > 10;
+        const obj = { one: 1, two: 2, three: 3, four: 4 };
+
+        expect(filter(isGreateThanTen)(obj)).toEqual({});
+    });
+
+    // TODO Currently manually curried due to
+    // https://github.com/millsp/ts-toolbelt/issues/207
 });
--- src/object/filter.ts
+++ src/object/filter.ts
@@ -2,8 +2,10 @@
  *
  */
 
-type T = () => string;
+type T = <A extends unknown>(predicate: (value: A) => boolean) => (value: Record<string, A>) => Record<string, A>;
 
-const f: T = () => 'filter';
+const f: T = predicate => value => Object.fromEntries(
+    Object.entries(value).filter(keyValue => predicate(keyValue[1]))
+);
 
 export default f;
