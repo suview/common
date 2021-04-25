69499298d7d8695ffc8a00129cc6fec02d933454
Pete Smith <pete@petesmith.io>
2021-04-25 10:56:14 +0100

    Implements string.fuzzySearch

--- src/string/fuzzy-search.spec.ts
+++ src/string/fuzzy-search.spec.ts
@@ -1,7 +1,19 @@
 import fuzzySearch from './fuzzy-search';
 
 describe('string/fuzzySearch', () => {
-    it('does something', () => {
-        expect(fuzzySearch()).toEqual('fuzzy-search');
+    it('returns true if subject string contains search string', () => {
+        expect(fuzzySearch('subject string', 'ect str')).toEqual(true);
+    });
+
+    it('returns false if subject string does not contain search string', () => {
+        expect(fuzzySearch('subject string', 'no')).toEqual(false);
+    });
+
+    it('returns true if search characters appear in order in subject string', () => {
+        expect(fuzzySearch('subject string', 'subj str')).toEqual(true);
+    });
+
+    it('is curried', () => {
+        expect(fuzzySearch('subject string')('subj str')).toEqual(true);
     });
 });
--- src/string/fuzzy-search.ts
+++ src/string/fuzzy-search.ts
@@ -2,8 +2,22 @@
  *
  */
 
-type T = () => string;
+import Curried from '../core/type/curried';
+import curry from '../core/curry';
+import pipeValue from '../core/pipe-value';
+import split from './split';
+import join from '../array/join';
+import matches from '../regex/matches';
 
-const f: T = () => 'fuzzy-search';
+type T = Curried<(haystack: string, needle: string) => boolean>;
+
+const f: T = curry((haystack, needle) => {
+    const regex: string = pipeValue(
+        needle,
+        split(''),
+        join('.*')
+    );
+    return matches(regex, haystack);
+});
 
 export default f;
