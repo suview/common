c89cb8cdaa75b92dc3eaff55ee1b08310ddc4c64
Pete Smith <pete@petesmith.io>
2021-08-26 16:44:58 +0100

    Adds array/reduceSome

---
+++ src/array/reduce-some.spec.ts
@@ -0,0 +1,42 @@
+import reduceSome from './reduce-some';
+
+describe('array/reduceSome', () => {
+    const add = (a: number, b: number) => a + b;
+    const isEven = (x: number) => x % 2 === 0;
+
+    it('reduces consecutive values which are identified by predicate', () => {
+        const input = [1, 2, 4, 2, 1];
+        const expectedOutput = [1, 8, 1];
+
+        expect(reduceSome(isEven, add, 0, input)).toEqual(expectedOutput);
+    });
+
+    it('reduces multiple consecutive series', () => {
+        const input = [1, 2, 4, 2, 1, 2, 4, 2, 1];
+        const expectedOutput = [1, 8, 1, 8, 1];
+
+        expect(reduceSome(isEven, add, 0, input)).toEqual(expectedOutput);
+    });
+
+    it('reduces series at the start and end of input array', () => {
+        const input = [2, 4, 1, 2, 4];
+        const expectedOutput = [6, 1, 6];
+
+        expect(reduceSome(isEven, add, 0, input)).toEqual(expectedOutput);
+    });
+
+    it('returns input unchanged if no elements match predicate', () => {
+        const input = [1, 3, 5];
+
+        expect(reduceSome(isEven, add, 0, input)).toEqual(input);
+    });
+
+    it('reduces entire input if all elements match predicate', () => {
+        const input = [2, 4, 6];
+        const expectedOutput = [12];
+
+        expect(reduceSome(isEven, add, 0, input)).toEqual(expectedOutput);
+    });
+
+    // TODO Curry
+});
---
+++ src/array/reduce-some.ts
@@ -0,0 +1,27 @@
+/**
+ *
+ */
+type Predicate<A> = (x: A) => boolean;
+type Callback<A> = (first: A, second: A) => A;
+
+type T = <A>(predicate: Predicate<A>, callback: Callback<A>, initialValue: A, data: A[]) => A[];
+
+// TODO This could be much more functional when some other functions exist
+const f: T = <A>(predicate: Predicate<A>, callback: Callback<A>, initialValue: A, data: A[]) => {
+    const output: A[] = [];
+    for (let i = 0; i < data.length; i += 1) {
+        let nonMatchIndex = data.slice(i).findIndex(x => !predicate(x));
+        if (nonMatchIndex === -1) nonMatchIndex = data.slice(i).length;
+        if (nonMatchIndex + i === i) {
+            output.push(data[i]);
+        } else {
+            const matches = data.slice(i, nonMatchIndex + i);
+            const reduced = matches.reduce(callback, initialValue);
+            output.push(reduced);
+            i += matches.length - 1;
+        }
+    }
+    return output;
+};
+
+export default f;
