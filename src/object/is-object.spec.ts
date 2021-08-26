c2b8c5a98b175206081802d55cfdf1baf2dfeb4b
Pete Smith <pete@petesmith.io>
2021-08-26 16:08:47 +0100

    Implements object/isArray

--- src/object/is-object.spec.ts
+++ src/object/is-object.spec.ts
@@ -1,7 +1,25 @@
 import isObject from './is-object';
 
 describe('object/isObject', () => {
-    it('does something', () => {
-        expect(isObject()).toEqual('is-object');
+    it('returns true if value is a simple object', () => {
+        expect(isObject({})).toEqual(true);
+    });
+
+    it('returns true if value is a class instance', () => {
+        expect(isObject(new Date())).toEqual(true);
+    });
+
+    it('returns false if value is null', () => {
+        expect(isObject(null)).toEqual(false);
+    });
+
+    it('returns false if value is array', () => {
+        expect(isObject([])).toEqual(false);
+    });
+
+    it('returns false if value is primitive', () => {
+        expect(isObject('string')).toEqual(false);
+        expect(isObject(123)).toEqual(false);
+        expect(isObject(true)).toEqual(false);
     });
 });
