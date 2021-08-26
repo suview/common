ed0f75346879f0b07e827ce09a17f460847cd1ce
Pete Smith <pete@petesmith.io>
2021-08-26 16:08:19 +0100

    Implements array/tail

--- src/array/tail.spec.ts
+++ src/array/tail.spec.ts
@@ -1,7 +1,15 @@
 import tail from './tail';
 
 describe('array/tail', () => {
-    it('does something', () => {
-        expect(tail()).toEqual('tail');
+    it('returns all but the first element of an array', () => {
+        expect(tail([1, 2, 3])).toEqual([2, 3]);
+    });
+
+    it('returns an empty array if provided array contains single element', () => {
+        expect(tail([1])).toEqual([]);
+    });
+
+    it('returns an empty array if provided array contains no elements', () => {
+        expect(tail([])).toEqual([]);
     });
 });
