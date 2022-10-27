a6b0d9a2cc7117509a1619221105914b67337a33
Pete Smith <pete@petesmith.io>
2022-10-27 19:26:42 +0100

    Adds Jest testing
    - Adds Jest, Babel, core-js dependencies
    - Adds Jest config
    - Adds Babel config
    - Adds npm script
    - Updates single function and spec to ensure there is a test

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    collectCoverage: false,
+    coverageDirectory: 'coverage',
+    coverageProvider: 'v8',
+    collectCoverageFrom: ['src/**/*.ts'],
+    coveragePathIgnorePatterns: ['src\/[^/]+\/type'],
+    coverageReporters: ['text'],
+    coverageThreshold: {
+        global: {
+            branches: 97,
+            functions: 97,
+            lines: 97,
+            statements: 97
+        }
+    },
+    moduleDirectories: [
+        'node_modules',
+        'src'
+    ],
+    moduleFileExtensions: [
+        'js',
+        'ts'
+    ]
+};
