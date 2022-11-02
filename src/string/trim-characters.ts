6293fd0f4f0c1542d6c14a4ebe73d532d52a0415
Pete Smith <pete@petesmith.io>
2022-11-02 01:10:34 +0000

    Implements string/trimCharacters
    Feature change

--- src/string/trim-characters.ts
+++ src/string/trim-characters.ts
@@ -2,10 +2,17 @@
  *
  */
 
+import { contains } from 'array';
+
 type T = (characters: string) => (value: string) => string;
 
-const f: T = _ => _ => 'trim-characters';
+const f: T = characters => value => {
+    // TODO Use firstCharacter, lastCharacter, front
+    // and tail string functions when available
+    const chars = characters.split('');
+    if (contains (value[0]) (chars)) return f (characters) (value.slice(1));
+    if (contains (value[value.length - 1]) (chars)) return f (characters) (value.slice(0, -1));
+    return value;
+};
 
 export default f;
-
-export {};
